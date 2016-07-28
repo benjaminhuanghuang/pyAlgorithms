cb59e90a8627c923d932a5c04d97244fffe10383
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-07-28 16:38:37 -0700

    update select_sort

--- sort/select_sort.py
+++ sort/select_sort.py
@@ -1,14 +1,26 @@
 '''
-1. find min element in the
+1. find the smallest item in the array and exchange it with the first entry
+    (itself if the first entry is already the smallest)
+2. find the next smallest item and exchange it with the second entry
+
+3. Continue in this way until the entire array is sorted
 '''
 
+from utilities.data_generator import *
 
 def select_sort(items):
     n = len(items)
     for i in range(0, n):
-        min = i  # 最小元素下标标记
+        min_index = i   # index of minimal entry
         for j in range(i + 1, n):
-            if items[j] < items[min]:
-                min = j  # 找到最小值的下标
-        items[min], items[i] = items[i], items[min]  # swap
+            if items[j] < items[min_index]:
+                min_index = j
+        items[min_index], items[i] = items[i], items[min_index]  # swap
     return items
+
+
+if __name__ == "__main__":
+    random_nums = generate_random_list()
+    print random_nums
+
+    print select_sort(random_nums)
