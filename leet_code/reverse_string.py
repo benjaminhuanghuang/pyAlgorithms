39e41c1bc252557b19a0bddc6b17b2dcb99ea515
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-07-29 16:06:33 -0700

    add comments

--- leet_code/reverse_string.py
+++ leet_code/reverse_string.py
@@ -3,6 +3,19 @@
 '''
 
 
+def reverse_string(str):
+    char_list = list(str)
+    start = 0
+    end = len(char_list) - 1
+    while end > start:
+        char_list[start], char_list[end] = char_list[end], char_list[start]
+        start += 1
+        end -= 1
+
+    return "".join(char_list)
+
+
+# Lazy solution
 def reverse_string_pythonic(input_str):
     '''
     This function use Python's slice notation
@@ -12,8 +25,19 @@ def reverse_string_pythonic(input_str):
     '''
     return input_str[::-1]
 
-def reverse_string(input_str):
+
+# Naive solution!
+def reverse_string_naive(input_str):
     result = ""
-    for i in xrange(len(input_str)-1, -1, -1):
+    for i in xrange(len(input_str) - 1, -1, -1):
         result += input_str[i]
     return result
+
+
+str = "123456"
+
+print reverse_string(str)
+
+str2 = "123456"
+# TypeError: 'str' object does not support item assignment
+str2[1], str2[4] = str2[4], str2[1]
