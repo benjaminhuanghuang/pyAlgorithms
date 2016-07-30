3837f737f48da5a1a893dd9c35d1f9e0708ff846
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-07-29 17:32:20 -0700

    add files

---
+++ README.md
@@ -0,0 +1,5 @@
+Algorithms Python Implementation
+=====================================
+Reference
+- LeetCode-Python-Solution
+    https://github.com/zhouchong90/LeetCode-Python-Solution
--- leet_code/remove_duplicates_from_sorted_array.py
+++ leet_code/remove_duplicates_from_sorted_array.py
@@ -10,7 +10,8 @@ Given input array nums = [1,1,2],
 
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 
-Subscribe to see which companies asked this question
+reference:
+https://github.com/zhouchong90/LeetCode-Python-Solution/
 
 '''
 
@@ -21,17 +22,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if nums is None or len(nums) <= 0:
-            return nums
+        length = len(nums)
+        if nums is None:
+            return 0
+        if length < 2:
+            return length
         size = 0
-        for i in xrange(len(nums)):
-            if i == 0 or nums[i] != nums[i - 1]:
-                nums[size] = nums[i]
+        for i in xrange(1, length):
+            if nums[i] != nums[size]:
                 size += 1
-        return size
+                nums[size] = nums[i]
+        return size + 1
 
 
 nums = [1, 1, 1, 1, 2, 3, 3, 4, 5]
+nums = [1, 2]
 
 s = Solution()
 print s.removeDuplicates(nums)
+print nums
