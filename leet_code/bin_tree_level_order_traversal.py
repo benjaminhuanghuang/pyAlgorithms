4d9f0f54cd37b89a4fb698f8b34991caf64e02ea
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-07-29 17:59:18 -0700

    add bin tree level traversal

--- leet_code/bin_tree_level_order_traversal.py
+++ leet_code/bin_tree_level_order_traversal.py
@@ -1,5 +1,44 @@
 '''
 102. Binary Tree Level Order Traversal
 
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
 
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
 '''
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        if root is None:
+            return result
+
+        nodes = [root]
+        while len(nodes):
+            val = []
+            children = []
+            for node in nodes:
+                val.append(node.val)
+                if node.left:
+                    children.append(node.left)
+                if node.right:
+                    children.append(node.right)
+            nodes = children
+            result.append(val)
+        return result
