cb4a1ca9a0b33d3418a0343a319674d4d14392cf
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-07-31 14:07:11 -0700

    add solution

--- utilities/data_generator.py
+++ utilities/data_generator.py
@@ -3,6 +3,18 @@
 '''
 import random
 
+from data_structure.tree_node import TreeNode
+
 def generate_random_list():
     random_items = [random.randint(-50, 100) for c in range(32)]
     return random_items
+
+
+def genertate_bin_tree(nums):
+    ""
+    if not nums or len(nums) < 1:
+        return None
+
+    root = TreeNode()
+
+    return root
