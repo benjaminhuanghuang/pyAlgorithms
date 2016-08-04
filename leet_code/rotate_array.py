643e987d4c1e68c1013a11bf21bf4f76f066fa3c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-04 16:52:25 -0700

    update rotate_array

--- leet_code/rotate_array.py
+++ leet_code/rotate_array.py
@@ -13,7 +13,7 @@ Try to come up as many solutions as you can, there are at least 3 different ways
 
 
 class Solution(object):
-    def rotate(self, nums, k):
+    def rotate_lazy(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -23,3 +23,52 @@ class Solution(object):
         k = k % n
         # !! Do not use nums = nums[n - k:] + nums[:n - k]
         nums[:] = nums[n - k:] + nums[:n - k]
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+    # time O(n) space O(1)
+    def rotate(self, nums, k):
+        if not nums:
+            return
+
+        if k <= 0:
+            return
+
+        n = len(nums)
+        k %= n
+
+        # reverse first part
+        self.reverse(nums, 0, n - k - 1)
+        # reverse second part
+        self.reverse(nums, n - k, n - 1)
+        # reverse whole list
+        self.reverse(nums, 0, n - 1)
+
+    # time O(kn) space O(1)
+    def rotate_3(self, nums, k):
+        if not nums:
+            return
+
+        if k <= 0:
+            return
+
+        n = len(nums)
+        k %= n
+        t = 0
+        while t < k:
+            last = nums[-1]
+            for i in xrange(n - 1, 0, -1):
+                nums[i] = nums[i - 1]
+            nums[0] = last
+            t += 1
+
+
+s = Solution()
+nums = [1, 2, 3, 4, 5, 6, 7]
+s.rotate_3(nums, 3)
+
+print nums
