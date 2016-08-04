deee23f8adf9b5078ab4ab344636c24c07055e3c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-04 10:47:05 -0700

    single number III

--- leet_code/single_number_III.py
+++ leet_code/single_number_III.py
@@ -27,6 +27,7 @@ class Solution(object):
             a_xor_b ^= nums[i]
 
         # ! Key point
+        # -a_xor_b = ~a_xor_b + 1
         a_xor_b &= -a_xor_b  # Get the last set (1) bit, that is the last difference bit those two number have
 
         result = [0] * 2
@@ -43,3 +44,6 @@ nums = [1, 2, 1, 3, 2, 5]
 s = Solution()
 
 print s.singleNumber(nums)
+
+
+print ~3 +1
