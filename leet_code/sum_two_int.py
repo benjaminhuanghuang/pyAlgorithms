3d9c94c5a0849eb905cf7366032e032c5c8fe642
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-11 10:41:42 -0700

    add comments in sum two int

--- leet_code/sum_two_int.py
+++ leet_code/sum_two_int.py
@@ -38,17 +38,6 @@ int getSum(int a, int b) {
 '''
 
 
-# Cause infinite loop, because Python will convert int to long int when the left-shift result too big
-# can not pass (-1,1)
-def get_sum_1(a, b):
-    # b is carry , a is bit sum
-    while b:
-        print b
-        print "\n"
-        a, b = (a ^ b), (a & b) << 1
-    return a
-
-
 def get_sum(a, b):
     """
     :type a: int
@@ -59,9 +48,23 @@ def get_sum(a, b):
     MASK = 0xFFFFFFFF
     while b:
         a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
-    # https://www.hrwhisper.me/leetcode-sum-two-integers/
-    # return a if a <= MAX_INT else ~(a & MAX_INT) ^ MAX_INT
+
+    # if a > MAX_INT means it is a negative number
+    # a & MAX_INT  cut 32 bits, but sign bit was changed to 0
+    # ~(a & MAX_INT)  change single bit(s) to 1, but inverse all rest bits
+    # ~(a & MAX_INT) ^ MAX_INT inverse all rest bits, because all bits in MAX_INT are 1s,  1^0 =1, 1^1=0
     return a if a <= MAX_INT else ~(a & MAX_INT) ^ MAX_INT
+
+
+# Cause infinite loop, because Python will convert int to long int when the left-shift result too big
+# can not pass (-1,1)
+def get_sum_c(a, b):
+    # b is carry , a is bit sum
+    while b:
+        print b
+        print "\n"
+        a, b = (a ^ b), (a & b) << 1
+    return a
 
 
 # Wrong solution
@@ -70,7 +73,7 @@ def get_sum_my(a, b):
     MASK = 0xFFFFFFFF
     while b:
         a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
-    #
+    #  0xFFFFFFFF80000000 will change data to long
     return a if a <= MAX_INT else ((a & MAX_INT) | 0xFFFFFFFF80000000)
 
 
@@ -80,13 +83,13 @@ print get_sum_my(-1, 1)
 # 0x7FFFFFFFFFFFFFFF
 #
 import sys
+
 print "Biggest int"
 print sys.maxint
 print bin(9223372036854775807)
 print hex(9223372036854775807)
-print -sys.maxint-10
-print bin(-sys.maxint-1)
-
+print -sys.maxint - 10
+print bin(-sys.maxint - 1)
 
 print bin(-2)
 print bin(-2 & 0x7fffffff)
@@ -97,7 +100,6 @@ print bin(18446744073709551614)
 print type(0x7FFFFFFFFFFFFFFF)
 print type(0xFFFFFFFFFFFFFFFF)
 
-
 #
 # print "-- test --"
 print get_sum_my(-1, 1)
@@ -105,4 +107,3 @@ print get_sum_my(2, 1)
 print get_sum_my(-1, -1)
 
 print type(18446744073709551614)
-
