022764e62a6bb0033649e51dedbecf4cb6aac4ec
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-14 15:09:52 -0700

    construct bin tree

--- leet_code/bin_tree_from_preorder_inorder.py
+++ leet_code/bin_tree_from_preorder_inorder.py
@@ -7,37 +7,17 @@ from data_structure.tree_node import TreeNode
 
 
 class Solution(object):
-    # Memory Limit Exceeded
-    def buildTree_9(self, preorder, inorder):
+    def buildTree(self, preorder, inorder):
         """
         :type preorder: List[int]
         :type inorder: List[int]
         :rtype: TreeNode
         """
         # write your code here
-        if not inorder:
-            return None  # inorder is empty
-        root = TreeNode(preorder[0])
-        rootPos = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1: 1 + rootPos], inorder[: rootPos])
-        root.right = self.buildTree(preorder[rootPos + 1:], inorder[rootPos + 1:])
-        return root
-
-    # http://jelices.blogspot.com/2014/05/leetcode-python-construct-binary-tree.html
-    def buildTree(self, preorder, inorder):
-        return self.buildTreeRec(preorder, inorder, 0, 0, len(preorder))
-
-    def buildTreeRec(self, preorder, inorder, indPre, indIn, element):
-        if element == 0:
-            return None
-        solution = TreeNode(preorder[indPre])
-        numElementsLeftSubtree = 0;
-        for i in range(indIn, indIn + element):
-            if inorder[i] == preorder[indPre]:
-                break
-            numElementsLeftSubtree += 1
-        solution.left = self.buildTreeRec(preorder, inorder, indPre + 1,
-                                          indIn, numElementsLeftSubtree)
-        solution.right = self.buildTreeRec(preorder, inorder, indPre + numElementsLeftSubtree + 1,
-                                           indIn + numElementsLeftSubtree + 1, element - 1 - numElementsLeftSubtree)
-        return solution
+        if inorder:
+            root_pos = inorder.index(preorder[0])
+            del preorder[0]
+            root = TreeNode(inorder[root_pos])
+            root.left = self.buildTree(preorder, inorder[:root_pos])
+            root.right = self.buildTree(preorder, inorder[root_pos + 1:])
+            return root
