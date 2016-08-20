0bae6fa67ccf49e1f8e1c491cf39c3d08441677a
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-20 12:11:50 -0700

    Add comments for search rotated sorted array

--- leet_code/search_rotated_sorted_array.py
+++ leet_code/search_rotated_sorted_array.py
@@ -9,16 +9,16 @@ You are given a target value to search. If found in the array return its index, 
 
 You may assume no duplicate exists in the array.
 
+Reference:
+    Cracking the Coding Interview   Ch6 P32
+    Cracking the Coding Interview   9.11-11.3 P256
+
 '''
 
 
 class Solution(object):
     # http://www.cnblogs.com/Dabay/p/4269053.html
-    # if mid > left, means left side is raising
-    #   if target in left section, search left, or search right
-    # if mid < left,
-    #   if target in right section, search left, or search right
-    def search_bs(self, nums, target):
+    def search(self, nums, target):
         if len(nums) == 1:
             return [-1, 0][nums[0] == target]
 
@@ -43,10 +43,11 @@ class Solution(object):
             else:
                 return -1
 
-    # http://www.cnblogs.com/Dabay/p/4269053.html
-    # if target < first num, search nums from left to right
-    # if target > fist num, search nums from right to left
-    def search(self, nums, target):
+    # The elements in the list goes in increasing order, then drop down, and increase again.
+    # The minimum element is the reset point
+    # if mid > right, the reset point will be in right half
+    # if mid < right, the reset point will be in left half
+    def search_bs(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -62,15 +63,26 @@ class Solution(object):
             mid = nums[m]
             if mid == target:
                 return m
+
+
             if mid > nums[left]:
+                # the element in left half goes in increasing order
                 if nums[left] < target and target < nums[m]:
+                    # target in left half
                     right = m - 1
                 else:
                     left = m + 1
             else:
+                # the reset is in left half, the element in right half goes in increasing order
                 if nums[m] < target and target < nums[right]:
+                    # target in right half
                     left = m + 1
                 else:
                     right = m - 1
         else:
             return -1
+
+
+s = Solution()
+print s.search_bs([4, 5, 6, 7, 0, 1, 2], 1)
+print s.search_bs([1, 3], 3)
