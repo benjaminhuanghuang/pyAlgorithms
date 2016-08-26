2165a3bcbdb3ae7fd68ab88797f71bbbb25e1a95
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-25 17:39:20 -0700

    first unique char in string

--- leet_code/course_schedule.py
+++ leet_code/course_schedule.py
@@ -23,6 +23,7 @@ a graph is represented.
 
 '''
 
+
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
@@ -30,3 +31,24 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+        degrees = [0] * numCourses
+        childs = [[] for x in range(numCourses)]
+        for pair in prerequisites:
+            degrees[pair[0]] += 1
+            childs[pair[1]].append(pair[0])
+
+        courses = set(range(numCourses))
+        flag = True
+
+        while flag and len(courses):
+            flag = False
+            removeList = []
+            for x in courses:
+                if degrees[x] == 0:
+                    for child in childs[x]:
+                        degrees[child] -= 1
+                    removeList.append(x)
+                    flag = True
+            for x in removeList:
+                courses.remove(x)
+        return len(courses) == 0
