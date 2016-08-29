443839ba0233b7cde9c7b30b395d95c0b6af1a64
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-08-29 14:34:09 -0700

    update

--- data_structure/tree_operations.py
+++ data_structure/tree_operations.py
@@ -1,3 +1,6 @@
+from tree_node import TreeNode
+
+
 def is_tree_same(t1, t2):
     if t1 is None and t2 is None:
         return True
@@ -71,3 +74,74 @@ def preorderTraversal(self, root):
     if root is None:
         return []
     return [root.val] + inorderTraversal(root.left) + inorderTraversal(root.right)
+
+
+def build_tree(nums):
+    if not nums:
+        return None
+
+    root = TreeNode(nums.pop(0))
+
+    stack = [root]
+    while stack and nums:
+        new_stack = []
+        for i in xrange(len(stack)):
+            if len(nums) <= 0:
+                return root
+            else:
+                left = TreeNode(nums.pop(0))
+                stack[i].left = left
+                new_stack.append(left)
+
+            if len(nums) <= 0:
+                return root
+            else:
+                right = TreeNode(nums.pop(0))
+                stack[i].right = right
+                new_stack.append(right)
+        stack = new_stack
+
+    return root
+
+
+def printTree(root):
+    queue = list()
+    queue.append(root)
+    res = []
+    while queue:
+        rowVal = []
+        tmpRow = []
+        for node in queue:
+            if node:
+                rowVal.append(node.val)
+                tmpRow.append(node.left)
+                tmpRow.append(node.right)
+        queue = tmpRow
+        if rowVal:
+            res.append(rowVal)
+    return res
+
+
+def printTree_2(root):
+    level = [root]
+    res = []
+    while level:
+        next_level = []
+        level_val =[]
+        for node in level:
+            if node:
+                level_val.append(node.val)
+                next_level.append(node.left)
+                next_level.append(node.right)
+        level = next_level
+        if level_val:
+            res.append(level_val)
+    return res
+
+
+tree = build_tree([1, 2, 3, 4, 5, None, 6])
+
+print_tree = printTree_2(tree)
+
+for i in print_tree:
+    print i
--- math/prime.py
+++ math/prime.py
@@ -3,3 +3,18 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+
+def is_prime_2(n):
+    for i in range(2, n / 2 + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for i in xrange(10000):
+    if is_prime(i) == is_prime_2(i):
+        pass
+        print "pass ", i
+    else:
+        print "not pass ", i
