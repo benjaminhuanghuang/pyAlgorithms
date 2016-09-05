80b1726f8ba91bd950fa3c7eeddf226f6cfa264a
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-04 23:14:28 -0700

    bin tree z order

---
+++ leet_code/copy_list_with_random_pointer.py
@@ -0,0 +1,15 @@
+'''
+138. Copy List with Random Pointer
+
+A linked list is given such that each node contains an additional random pointer which could point to any node
+in the list or null.
+
+Return a deep copy of the list.
+'''
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
