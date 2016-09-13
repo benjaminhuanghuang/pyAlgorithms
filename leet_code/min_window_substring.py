6a91f82136aafe511078c9579e656b56db9356d3
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-12 20:39:29 -0700

    merge intervals

--- leet_code/min_window_substring.py
+++ leet_code/min_window_substring.py
@@ -15,13 +15,68 @@ If there are multiple such windows, you are guaranteed that there will always be
 
 '''
 
+import collections
+
+
 class Solution(object):
+    # http://www.tangjikai.com/algorithms/leetcode-76-minimum-window-substring
     def minWindow(self, s, t):
+        res = ''
+        window = []
+        dic = collections.defaultdict(list)
+        tCount = collections.Counter(t)
+
+        for i, c in filter(lambda x: x[1] in t, enumerate(s)):
+            if len(dic[c]) == tCount[c]:
+                window.remove(dic[c].pop(0))
+
+            dic[c].append(i)
+            window.append(i)
+
+            if len(window) == len(t) and (res == '' or window[-1] - window[0] < len(res)):
+                res = s[window[0]: window[-1] + 1]
+
+        return res
+
+    def minWindow_9(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: str
         """
-        if not t:
-            return ""
 
+        if (t == ""):
+            return ""
+        d, dt = {}, dict.fromkeys(t, 0)
+        for c in t:
+            d[c] = d.get(c, 0) + 1
+        pi, pj, cont = 0, 0, 0
+        if (s == "" or t == ""):
+            return ""
+        ans = ""
+        while pj < len(s):
+            if s[pj] in dt:
+                if dt[s[pj]] < d[s[pj]]:
+                    cont += 1
+                dt[s[pj]] += 1
+            if cont == len(t):
+                while pi < pj:
+                    if s[pi] in dt:
+                        if dt[s[pi]] == d[s[pi]]:
+                            break
+                        dt[s[pi]] -= 1
+                    pi += 1
+                if ans == '' or pj - pi < len(ans):
+                    ans = s[pi:pj + 1]
+                dt[s[pi]] -= 1
+                pi += 1
+                cont -= 1
+            pj += 1
+        return ans
+
+
+source = "ADOBECODEBANC"
+target = "ABC"
+
+s = Solution()
+print s.minWindow(source, target)
--- leet_code/range_sum_query_mutable.py
+++ leet_code/range_sum_query_mutable.py
@@ -17,6 +17,7 @@ You may assume the number of calls to update and sumRange function is distribute
 
 reference:
 http://www.cnblogs.com/yrbbest/p/5056739.html
+http://bookshadow.com/weblog/2015/11/18/leetcode-range-sum-query-mutable/
 
 Binary Index Tree / Fenwick Tree
 https://www.zybuluo.com/Yano/note/320858
@@ -27,12 +28,33 @@ http://www.cnblogs.com/shuaiwhu/archive/2012/04/22/2464583.html
 '''
 
 
+# Binary Indexed Tree / Fenwick Tree
 class NumArray(object):
     def __init__(self, nums):
         """
         initialize your data structure here.
         :type nums: List[int]
         """
+        self.sums = [0] * (len(nums) + 1)
+        self.nums = nums
+        self.n = len(nums)
+        for i in xrange(len(nums)):
+            self.add(i + 1, nums[i])
+
+    def add(self, x, val):
+        while x <= self.n:
+            self.sums[x] += val
+            x += self.lowbit(x)
+
+    def lowbit(self, x):
+        return x & -x
+
+    def sum(self, x):
+        res = 0
+        while x > 0:
+            res += self.sums[x]
+            x -= self.lowbit(x)
+        return res
 
     def update(self, i, val):
         """
@@ -40,6 +62,8 @@ class NumArray(object):
         :type val: int
         :rtype: int
         """
+        self.add(i + 1, val - self.nums[i])
+        self.nums[i] = val
 
     def sumRange(self, i, j):
         """
@@ -48,3 +72,90 @@ class NumArray(object):
         :type j: int
         :rtype: int
         """
+        if not self.nums:
+            return 0
+        return self.sum(j + 1) - self.sum(i)
+
+
+class Segment_Tree(object):
+    def add(self, start, end, llist):
+        self.start = start
+        self.end = end
+        self.middle = (start + end) / 2
+        self.num = 0
+
+        if self.start + 1 == self.end:
+            self.num = llist[self.start]
+            return self.num
+        else:
+            left = Segment_Tree()
+            self.left = left
+            self.num += left.add(start, self.middle, llist)
+
+            right = Segment_Tree()
+            self.right = right
+            self.num += right.add(self.middle, end, llist)
+
+            return self.num
+
+    def update(self, index, num):
+        if self.start == index and self.start + 1 == self.end:
+            cha = num - self.num
+            self.num = num
+            return cha
+
+        if index >= self.middle:
+            cha = self.right.update(index, num)
+            self.num += cha
+            return cha
+        else:
+            cha = self.left.update(index, num)
+            self.num += cha
+            return cha
+
+    def sum(self, start, end):
+        if start == self.start and end == self.end:
+            return self.num
+
+        if end <= self.middle:
+            return self.left.sum(start, end)
+        elif start >= self.middle:
+            return self.right.sum(start, end)
+        else:
+            left_val = self.left.sum(start, self.middle)
+            right_val = self.right.sum(self.middle, end)
+
+            return left_val + right_val
+
+
+class NumArray_Segment_Tree(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+
+        if len(nums) == 0:
+            nums.append(0)
+
+        self.nums = nums
+        self.st = Segment_Tree()
+        self.st.add(0, len(self.nums), self.nums)
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        self.st.update(i, val)
+        return val
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.st.sum(i, j + 1)
