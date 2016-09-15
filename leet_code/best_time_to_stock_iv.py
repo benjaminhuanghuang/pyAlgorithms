fbcad38db7a6d1c601da54617d7a0df8689157ed
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-15 12:46:56 -0700

    best time to stock IV

--- leet_code/best_time_to_stock_iv.py
+++ leet_code/best_time_to_stock_iv.py
@@ -11,9 +11,32 @@ You may not engage in multiple transactions at the same time (ie, you must sell 
 
 
 class Solution(object):
+    # https://www.hrwhisper.me/leetcode-best-time-to-buy-and-sell-stock-i-ii-iii-iv/
     def maxProfit(self, k, prices):
         """
         :type k: int
         :type prices: List[int]
         :rtype: int
         """
+        size = len(prices)
+        if k >= size / 2:
+            return self.quickSolve(size, prices)
+
+        # dp[i][j] = trade[i] at day[j]
+        # dp[i][j]=max(dp[i][j-1],prices[j] + maxTemp)
+        dp = [[0 for j in xrange(size)] for i in xrange(k + 1)]
+
+        for i in xrange(1, k + 1):
+            maxTemp = -prices[0]
+            for j in xrange(1, size):
+                dp[i][j] = max(dp[i][j - 1], prices[j] + maxTemp)
+                maxTemp = max(maxTemp, dp[i - 1][j - 1] - prices[j])
+        return dp[k][size - 1]
+
+    # refer to: Best Time to Buy and Sell Stock II
+    def quickSolve(self, size, prices):
+        sum = 0
+        for x in range(size - 1):
+            if prices[x + 1] > prices[x]:
+                sum += prices[x + 1] - prices[x]
+        return sum
