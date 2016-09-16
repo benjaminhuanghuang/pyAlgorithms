a2a952fa857a36d8099f3937dc3b469140cd4711
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-16 15:58:36 -0700

    count of smaller number after self

--- leet_code/count_of_smaller_number_after_self.py
+++ leet_code/count_of_smaller_number_after_self.py
@@ -13,12 +13,41 @@ To the right of 2 there is only 1 smaller element (1).
 To the right of 6 there is 1 smaller element (1).
 To the right of 1 there is 0 smaller element.
 Return the array [2, 1, 1, 0].
+
+Reference:
+    # http://bookshadow.com/weblog/2015/12/06/leetcode-count-of-smaller-numbers-after-self/
 '''
 
-# http://bookshadow.com/weblog/2015/12/06/leetcode-count-of-smaller-numbers-after-self/
+
 class Solution(object):
+    # http://bookshadow.com/weblog/2015/12/06/leetcode-count-of-smaller-numbers-after-self/
     def countSmaller(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+
+        def sort(enum):
+            half = len(enum) / 2
+            if half:
+                left, right = sort(enum[:half]), sort(enum[half:])
+                len_f, len_r = len(left), len(right)
+                l = r = 0
+                while l < len_f or r < len_r:
+                    if r == len_r or l < len_f and left[l][1] <= right[r][1]:
+                        enum[l + r] = left[l]
+                        result[left[l][0]] += r
+                        l += 1
+                    else:
+                        enum[l + r] = right[r]
+                        r += 1
+            return enum
+
+        result = [0] * len(nums)
+        sort(list(enumerate(nums)))
+        return result
+
+
+nums = [5, 2, 6, 1]
+s = Solution()
+print s.countSmaller(nums)
