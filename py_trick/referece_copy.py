838d5f6ec3d91531c11a79c7b4f206cf928644ef
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-20 00:17:13 -0700

    remove comments

--- py_trick/referece_copy.py
+++ py_trick/referece_copy.py
@@ -1,12 +1,13 @@
 n = 3
 # Note! [[0] * n] * n has same reference!
 matrix1 = [[0] * n] * n
-
-matrix = [[0 for i in range(n)] for j in range(n)]
-
 matrix1[0][1] = 1
-
 print matrix1
+
+# different reference
+matrix = [[0 for i in range(n)] for j in range(n)]
+matrix[0][1] = 2
+print matrix
 
 a = [1] * 2
 b = [a] * 2
