5114178fc9595df2e2af9ec2c697db8ddc860679
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-22 09:41:50 -0700

    more solutions

---
+++ google/find_max_xor.py
@@ -0,0 +1,7 @@
+'''
+Given a list of numbers, a[0], a[1], a[2], ... , a[N-1], where 0 <= a[i] < 2^32, find the maximum result of a[i] XOR a[j].
+
+reference:
+    http://www.geeksforgeeks.org/find-maximum-subset-xor-given-set/
+'''
+
