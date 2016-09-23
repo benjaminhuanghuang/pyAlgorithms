da73bed3b2a133876ea478bda02c659dcae69a99
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-23 14:07:16 -0700

    max rectangle

--- leet_code/self_crossing.py
+++ leet_code/self_crossing.py
@@ -1,4 +1,5 @@
 '''
+335. Self Crossing
 
 You are given an array x of n positive numbers. You start at point (0,0) and moves x[0] metres to the north, then x[1]
 metres to the west, x[2] metres to the south, x[3] metres to the east and so on. In other words, after each move
@@ -34,7 +35,7 @@ class Solution(object):
             # Case 2: current line overlapped line i-4
             elif i >= 4 and x[i - 1] == x[i - 3] and x[i] + x[i - 4] >= x[i - 2]:
                 return True
-            # Case 3: current line crosses the line i-6 steps ahead of it
+            # Case 3: current line crosses the line i-6
             elif i >= 5 and x[i - 2] >= x[i - 4] and x[i] + x[i - 4] >= x[i - 2] and \
                             x[i - 1] <= x[i - 3] and x[i - 1] + x[i - 6] >= x[i - 3]:
                 return True
