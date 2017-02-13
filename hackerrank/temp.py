0299a06bc361774a252cbec09411e388e40b55a6
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-02-13 10:39:03 -0800

    udpate

--- hackerrank/temp.py
+++ hackerrank/temp.py
@@ -2,6 +2,9 @@ import math
 
 
 def is_prime(num):
+    if num <= 1:
+        return False
+
     sqr = int(math.sqrt(num)) + 1  # +1 for num =2
     for i in range(2, sqr):
         if num % i == 0:
