7a6af975feb849de0211e04f66d076006a83f1e4
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-02-17 09:25:33 -0800

    more solution

--- leet_code/496_Next Greater Element I.py
+++ leet_code/496_Next Greater Element I.py
@@ -1,5 +1,5 @@
 '''
-496. Next Greater Element I
+496. Next Greater Element I  #503
 
 You are given two arrays (without duplicates) nums1 and nums2 where nums1's elements are subset of nums2.
 Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
@@ -25,7 +25,6 @@ All elements in nums1 and nums2 are unique.
 The length of both nums1 and nums2 would not exceed 1000.
 
 '''
-import collections
 
 # http://www.cnblogs.com/grandyang/p/6399855.html
 class Solution(object):
