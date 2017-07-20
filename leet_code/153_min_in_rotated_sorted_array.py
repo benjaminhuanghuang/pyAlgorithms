64bbe22b148984a0d928540fa11b0fef31f3256d
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-20 11:32:47 -0700

    update 153, 154

--- leet_code/153_min_in_rotated_sorted_array.py
+++ leet_code/153_min_in_rotated_sorted_array.py
@@ -1,5 +1,5 @@
 '''
-154. Find Minimum in Rotated Sorted Array II
+153. Find Minimum in Rotated Sorted Array
 
 Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 
@@ -7,35 +7,20 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 
 Find the minimum element.
 
-The array may contain duplicates.
-
-reference:
-    http://bangbingsyb.blogspot.com/2014/11/leecode-find-minimum-in-rotated-sorted.html
+You may assume no duplicate exists in the array.
 '''
 
 
 class Solution(object):
     def findMin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         low = 0
         high = len(nums) - 1
         while (low < high):
-            if(nums[low] < nums[high]):
+            if (nums[low] < nums[high]):
                 return nums[low]
             mid = low + (high - low) / 2
             if nums[mid] < nums[high]:
                 high = mid
-            elif (nums[mid] > nums[low]):
-                low = mid + 1
             else:
-                low += 1
+                low = mid + 1
         return nums[low]
-
-
-nums = [1, 1, 3]
-
-s = Solution()
-print s.findMin(nums)
